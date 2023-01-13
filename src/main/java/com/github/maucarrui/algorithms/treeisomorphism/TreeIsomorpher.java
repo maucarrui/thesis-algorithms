72ab7c07633ab27028e41a35d86a99d0f535d8be
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2023-01-13 16:55:43 -0600

    Added auxiliary method setInitialChildren for the Tree Isomorpher.
    Auxiliary method to append the leaves found on the 0-th level to its parent
    list.

--- src/main/java/com/github/maucarrui/algorithms/treeisomorphism/TreeIsomorpher.java
+++ src/main/java/com/github/maucarrui/algorithms/treeisomorphism/TreeIsomorpher.java
@@ -1,6 +1,7 @@
 package com.github.maucarrui.algorithms.treeisomorphism;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Class to represent a tree isomorpher.
@@ -15,6 +16,27 @@ public class TreeIsomorpher<U extends Comparable<U>, V extends Comparable<V>> {
      * to its methods.
      */
     public TreeIsomorpher(){}
+
+    /**
+     * Auxiliary method to append the leaves found on the 0-th level to its
+     * parent list.
+     * @param T the rooted tree.
+     * @param children the mapping of the ordered children.
+     */
+    private <K extends Comparable<K>> void
+	setInitialChildren(RootedTree<K> T, HashMap<K, LinkedList<K>> children) {
+	for (K leaf : T.getVerticesOfLevel(0)) {
+	    K parent = T.getParent(leaf);
+
+	    if (children.containsKey(parent)) {
+		children.get(parent).add(leaf);
+	    } else {
+		LinkedList<K> ch = new LinkedList<>();
+		ch.add(leaf);
+		children.put(parent, ch);
+	    }
+	}
+    }
 
     /**
      * Returns whether two rooted trees are isomorphic.
