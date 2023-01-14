444707d2138ff7ef2b9a6a5257a78b4dbf1714d6
Mauricio Carrasco Ruiz <maucarrui@ciencias.unam.mx>
2023-01-14 13:08:22 -0600

    Added a new unit test for the Tree Isomorpher.
    The new unit test tests the method areIsomorphic with some small hardcoded
    graphs as rooted trees.

--- src/test/java/com/github/maucarrui/algorithms/treeisomorphism/test/TestTreeIsomorpher.java
+++ src/test/java/com/github/maucarrui/algorithms/treeisomorphism/test/TestTreeIsomorpher.java
@@ -52,4 +52,31 @@ public class TestTreeIsomorpher {
 	isomorphism = TI.areIsomorphic(G, 0, H, "a");
 	Assert.assertNull(isomorphism);
     }
+
+    /**
+     * Test the areIsomorphic method with some small hardcoded graphs as rooted
+     * trees.
+     */
+    @Test
+    public void testAreIsomorphicRootedHardcodedSmall() {
+	/* Define a tree isomorpher to have access to its methods. */
+	TreeIsomorpher<Integer, String> TI = new TreeIsomorpher<>();
+	HashMap<Integer, String> isomorphism, expectedIsomorphism;
+
+	/* Test for two P2 (graphs that consist only of a path with 2
+	 * vertices). */
+	Graph<Integer> G = new Graph<>();
+	Graph<String> H = new Graph<>();
+	G.addVertex(0); G.addVertex(1); G.connectVertices(0, 1);
+	H.addVertex("a"); H.addVertex("b"); H.connectVertices("a", "b");
+
+	/* The expected isomorphism of the rooted trees. */
+	expectedIsomorphism = new HashMap<Integer, String>();
+	expectedIsomorphism.put(0, "a");
+	expectedIsomorphism.put(1, "b");
+
+	/* Check that the obtained isomorphism is correct. */
+	isomorphism = TI.areIsomorphic(G, 0, H, "a");
+	Assert.assertEquals(expectedIsomorphism, isomorphism);
+    }
 }
